a00e959f5a27a5aaa5cc1591034890c34894d0bf
Flild <flildman@yandex.ru>
2022-09-14 15:55:45 +0300

    Добавьте файлы проекта.

---
+++ NetologyTasks/CycleTask.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NetologyTasks
+{
+    public class CycleTask
+    {
+        public int CycleWhile(int numberToReverse)
+        {
+            int reverseNumber = 0;
+            while(numberToReverse > 0)
+            {
+                reverseNumber = reverseNumber*10 + numberToReverse % 10;
+                numberToReverse /= 10;
+            }
+            return reverseNumber;
+        }
+    }
+}
